3b2dbba1daee06f00bb3d4343537a322e55f03b7
AndersonTaramelli <andersontaramellidesouza@gmail.com>
2022-08-12 11:25:26 -0300

    Escola V4

---
+++ Assets/Scripts/JoystickCam.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class JoystickCam : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
+{
+    public Transform stick;
+    public Vector2 axis;
+    private Vector2 joystickSize2;
+
+    void Start()
+    {
+        joystickSize2 = GetComponent<RectTransform>().rect.max;
+    }
+
+    private void MoveStick(Vector2 pos)
+    {
+        stick.position = pos;
+        pos.x = Mathf.Clamp(stick.localPosition.x, 0, joystickSize2.x);
+        pos.y = Mathf.Clamp(stick.localPosition.y, 0, joystickSize2.y);
+        stick.localPosition = pos;
+
+        axis = ((stick.localPosition / joystickSize2) * 2) - Vector2.one;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        MoveStick(eventData.position);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        MoveStick(eventData.position);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        axis = Vector2.zero;
+        stick.localPosition = new Vector3(-50, -50, 0);
+    }
+}
---
+++ Assets/Scripts/JoystickVirtual.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class JoystickVirtual : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
+{
+    public Transform knob;
+    public Vector2 axis;
+    private Vector2 joystickSize;
+
+    void Start()
+    {
+        joystickSize = GetComponent<RectTransform>().rect.max;
+    }
+
+    private void MoveKnob(Vector2 pos)
+    {
+        knob.position = pos;
+        pos.x = Mathf.Clamp(knob.localPosition.x, 0, joystickSize.x);
+        pos.y = Mathf.Clamp(knob.localPosition.y, 0, joystickSize.y);
+        knob.localPosition = pos;
+
+        axis = ((knob.localPosition / joystickSize) * 2) - Vector2.one;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        MoveKnob(eventData.position);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        MoveKnob(eventData.position);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        axis = Vector2.zero;
+        knob.localPosition = new Vector3 (200, 200, 0);
+    }
+}
